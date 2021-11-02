b9c388a8d0e95fe6ccefd526f3d1c0364086cae6
ELNDRY <71252393+ELNDRY@users.noreply.github.com>
2021-11-02 23:54:30 +0700

    demo Task_1_4_1

---
+++ Calculator/src/main/java/ru/nsu/yadryshnikova/Arithmetic.java
@@ -0,0 +1,39 @@
+package ru.nsu.yadryshnikova;
+
+public class Arithmetic {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double sub(double a, double b) {
+        return a - b;
+    }
+
+    public static double mult(double a, double b) {
+        return a * b;
+    }
+
+    public static double div(double a, double b) {
+        return a / b;
+    }
+
+    public static double log(double a) {
+        return Math.log(a);
+    }
+
+    public static double pow(double a, double b) {
+        return Math.pow(a, b);
+    }
+
+    public static double sqrt(double a) {
+        return Math.sqrt(a);
+    }
+
+    public static double sin(double a) {
+        return Math.sin(a);
+    }
+
+    public static double cos(double a) {
+        return Math.cos(a);
+    }
+}
